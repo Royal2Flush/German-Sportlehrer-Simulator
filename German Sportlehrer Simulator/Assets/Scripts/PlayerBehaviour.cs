2e69bc827f798c22d1b5fb64cec3ed9f24639a54
Dean <deanjuerges@posteo.de>
2018-05-26 16:39:25 +0200

    Jetz aber

---
+++ German Sportlehrer Simulator/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour {
+
+	public GameObject PathBall;
+	public float maxForce;
+	public float minForce;
+
+	private float force = 2500.0f;
+	private float lastSpawn = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		//force = minForce;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Time.time - lastSpawn > 0.1f) {
+
+			// Raycast um Ziel zu finden
+			//RaycastHit hit;
+			//Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			//if (Physics.Raycast (ray, out hit, 100.0f)) {
+				//Debug.Log (hit.point);
+			//}
+
+			GameObject ball = Instantiate(PathBall, transform.position, Quaternion.identity);
+			ball.transform.rotation = this.gameObject.transform.rotation;
+			ball.GetComponent<Rigidbody> ().AddForce (ball.transform.forward * force);
+
+			lastSpawn = Time.time;
+		}
+	}
+}
---
+++ German Sportlehrer Simulator/Assets/Scripts/Teacher.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teacher : MonoBehaviour {
+
+
+
+    float throwIntensity = 0;
+    float throwTreshold = 30f;
+    public GameObject ball;
+
+    Rigidbody ballRigid;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+        float mousePos = Input.mousePosition.x;
+
+        float onePercent = Screen.width / 100;
+        float mousePosInPercent = mousePos / onePercent;
+
+        //0 is in the middle not buttom left
+        float rotateAmount = mousePosInPercent - 50;
+        //Rote the Player in 50 degree
+
+        this.transform.eulerAngles = new Vector3(this.transform.eulerAngles.x, rotateAmount/2, this.transform.eulerAngles.z);
+
+        Debug.Log(throwIntensity);
+
+        CalcThrowIntensity();
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            //throw ball
+            GameObject ThrowBall = Resources.Load<GameObject>("throwBall") as GameObject;
+            ThrowBall.transform.position = ball.transform.position;
+            ThrowBall.transform.rotation = transform.rotation;
+
+            GameObject newBall = Instantiate(ThrowBall);
+            ballRigid = newBall.GetComponent<Rigidbody>();
+            ballRigid.AddForce(transform.forward * throwIntensity,ForceMode.Impulse);
+
+            throwIntensity = 0;
+        }
+
+		
+	}
+
+
+    void CalcThrowIntensity()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            throwIntensity += 0.1f;
+
+        }
+
+        else{
+           // throwIntensity = 0;
+
+        }
+
+        if(throwIntensity > throwTreshold)
+        {
+            throwIntensity = throwTreshold;
+            //spiele anspamnnsound
+
+        }
+            
+
+
+    }
+}
