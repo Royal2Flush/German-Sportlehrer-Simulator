0fbe32bd1b18e3133431b2dce0be9f15b3cb9695
Tobias Kohl <tobiko10@aol.com>
2018-05-26 16:13:48 +0200

    Moving Pupils

---
+++ German Sportlehrer Simulator/Assets/Scripts/Person.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PersonState { idle, moving, sleeping, hit }
+
+public abstract class Person : MonoBehaviour {
+
+    protected PersonState state;
+    private Vector3 currentGoal;
+    private float currentMovementSpeed;
+
+	// Use this for initialization
+	void Start ()
+    {
+        state = PersonState.idle;
+        currentGoal = gameObject.transform.position;
+        currentMovementSpeed = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        switch (state)
+        {
+            case PersonState.idle:
+                UpdateIdle();
+                break;
+            case PersonState.moving:
+                UpdateMoving();
+                break;
+            case PersonState.sleeping:
+                UpdateSleeping();
+                break;
+            case PersonState.hit:
+                UpdateHit();
+                break;
+        }
+    }
+
+    public abstract void OnHit();
+    protected abstract void SpecificUpdate();
+    protected abstract void UpdateIdle();
+    protected abstract void UpdateMoving();
+    protected abstract void UpdateSleeping();
+    protected abstract void UpdateHit();
+    protected abstract void SetIdleTimer();
+
+    public void MoveInDirectionOnce(Vector3 direction, float speed)
+    {
+        gameObject.transform.Translate(Vector3.Normalize(direction) * speed * Time.deltaTime);
+    }
+
+    public void SetMovementGoal(Vector3 goal, float speed)
+    {
+        state = PersonState.moving;
+        currentGoal = goal;
+        currentMovementSpeed = speed;
+    }
+
+    public void SetMovementGoalAtRandom(float speed)
+    {
+        SetMovementGoal(new Vector3(Random.Range(-5f, 5f), 0, Random.Range(0f, 5f)), speed);
+    }
+
+    protected void Move()
+    {
+        if(state == PersonState.moving)
+        {
+            MoveInDirectionOnce(currentGoal - gameObject.transform.position, currentMovementSpeed);
+            if (Vector3.Distance(currentGoal, gameObject.transform.position) < currentMovementSpeed * Time.deltaTime)
+            {
+                gameObject.transform.position = currentGoal;
+                state = PersonState.idle;
+                SetIdleTimer();
+            }
+        }
+    }
+}
---
+++ German Sportlehrer Simulator/Assets/Scripts/Pupil.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pupil : Person {
+
+    public static float hitCooldownTimer;
+    public float movementSpeed;
+    private float timerToStateChange;
+    private const float IDLE_MIN = 0.5f;
+    private const float IDLE_MAX = 2f;
+
+    protected override void SpecificUpdate()
+    {
+        
+    }
+
+    public override void OnHit()
+    {
+        if (state == PersonState.sleeping)
+        {
+            Debug.Log("+1 point");
+        }
+        else
+        {
+            Debug.Log("-1 point");
+        }
+
+        state = PersonState.hit;
+        timerToStateChange = hitCooldownTimer;
+    }
+
+    protected override void UpdateIdle()
+    {
+        timerToStateChange -= Time.deltaTime;
+        if (timerToStateChange < 0)
+        {
+            SetMovementGoalAtRandom(movementSpeed);
+            state = PersonState.moving;
+        }
+    }
+
+    protected override void UpdateMoving()
+    {
+        base.Move();
+    }
+
+    protected override void UpdateSleeping()
+    {
+        
+    }
+
+    protected override void UpdateHit()
+    {
+        timerToStateChange -= Time.deltaTime;
+        if (timerToStateChange < 0)
+        {
+            SetIdleTimer();
+            state = PersonState.idle;
+        }
+        
+    }
+
+    protected override void SetIdleTimer()
+    {
+        timerToStateChange = Random.Range(IDLE_MIN, IDLE_MAX);
+    }
+
+}
